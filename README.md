6f3975246ed26290447a80de62b11f6b25fbdada
Harrish S <harrish.s@hotmail.ca>
2024-07-03 18:24:14 -0400

    add demo links

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# mernAuthStarter
+[Demo](https://www.loom.com/share/ff8baf7e050f4f688ad86aec8041c9b2)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,12 @@ import Layout from "./Components/Layout/Layout";
 function App() {
 	return (
 		<>
+			<a
+				href="https://www.loom.com/share/ff8baf7e050f4f688ad86aec8041c9b2"
+				target="_blank"
+			>
+				<h1>Demo</h1>
+			</a>
 			<Layout />
 		</>
 	);
